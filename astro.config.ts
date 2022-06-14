e8d4573d3c96948c5c7f221d6a826be2aa8bdddd
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-06-14 07:55:09 +0200

    feat: compress site

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
       external: ["svgo"],
     },
   },
-  integrations: [tailwind()],
+  integrations: [tailwind({}), compress()],
+  experimental: { integrations: true },
 });
